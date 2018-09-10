54d9d79b102dbb9bd330013cf7a13ebcec7e94c4
Rygel Orionis <mirco.centrone@gmail.com>
2018-09-10 20:48:30 +0200

    Commi1

---
+++ RygelOrionis.Exercise/RygelOrionis.Exercise/NegatePredicate.cs
@@ -0,0 +1,9 @@
+﻿namespace RygelOrionis.Exercise
+{
+    using System;
+
+    public static class NegatePredicate
+    {
+        public static Func<T, bool> Negate<T>(this Func<T, bool> f, Func<T, bool> pred) => t => !pred(t);
+    }
+}
